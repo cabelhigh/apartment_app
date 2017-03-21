058700284ca7ac0a4e179b7ee89be4bac0691c5d
Learn <learn@learn02.local>
2017-03-21 15:24:21 -0700

    Added Default Portraits to Users

--- ApartmentApp/app/models/user.rb
+++ ApartmentApp/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many(:apartments)
-  has_attached_file :avatar, styles: { small: "64x64", med: "100x100", large: "200x200" }
-validates_attachment :avatar, presence: true,
+  has_attached_file :avatar, styles: { small: "64x64", med: "100x100", large: "200x200" }, default_url: "http://placehold.it/150x175"
+validates_attachment :avatar,
     content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
     size: { in: 0..10.megabytes }
 end
